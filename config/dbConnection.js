33b7208209a73381bf09b2f9ccfa21643ec0440d
Amirouche DELMI <amirouche.delmi@gmail.com>
2024-02-01 15:54:19 +0100

    update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect("mongodb+srv://" + process.env.DB_USER_PASS + "@gestion-suivi-marches.lctzovu.mongodb.net/gestion-suivi-marches")
+    .connect("mongodb+srv://" + process.env.DB_USER_PASS + "@gestion-suivi-marches.lctzovu.mongodb.net/gestion-suivi-marches", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        })
     .then(() => console.log("Connected to MongoDB"))  
     .catch((err) => console.log("Failed to connect to MongoDB", err))
 
